acd4fffe5963aeb130f229ac379edc16e36d25fa
eal6gf <elum1234@gmail.com>
2017-12-21 16:20:15 -0500

    attraction controller and views

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,48 @@
+class AttractionsController < ApplicationController
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def new
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def show
+    @attraction = Attraction.new
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.valid?
+      @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      #add stuff for flash errors
+      redirect_to new_attraction_path
+    end
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.assign_attributes(attraction_params)
+    if @attraction.valid?
+      @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      flash[:error] = @attraction.errors.full_messages
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
+  end
+
+end
